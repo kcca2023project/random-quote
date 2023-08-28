9a45d368f504b416490d3767f9f0914526245870
jaycoleCyber <okoroaforrjames3@gmail.com>
2023-08-28 13:38:57 +0100

    Added new files

---
+++ src/Component/Nav.jsx
@@ -0,0 +1,38 @@
+// import React, {useState, useEffect} from 'react';
+// import project from '../project.jpg';
+
+// const Nav = () => {
+//     const [quote, setQuote] = useState([])
+//     useEffect(() => {
+// const fetchQuotes = async () =>{
+//     let response = await fetch()
+//     let data = await response.json()
+//     console.log(data);
+// }
+// fetchQuotes()
+//     })
+//   return (
+//     <div className="flex flex-col items-center fixed">
+//       <div className="bg-black text-white w-full p-4 text-center">
+//         <h2 className="font-bold text-6xl">JKid</h2>
+//       </div>
+//       <img src={project} alt="" className="w-screen max-h-[43em] " />
+//     </div>
+//   );
+// };
+
+// export default Nav;
+
+// import React from 'react'
+// import {ReactDOM} from 'react'
+// import App from '.App'
+
+// ReactDOM.render(App /  )
+
+// const Nav = () => {
+//   return (
+//     <div>Nav</div>
+//   )
+// }
+
+// export default Nav
